2d622b4ad4b9422adcf62fd5f52fcef5340e0363
Matej Risek <mrisek@gmail.com>
2019-05-27 16:40:26 +0200

    Don't run signing key decrypt script if no deployment will be made (#11)
    * Don't run signing key decrypt script if no deployment will be made
    
    * Add new line
    
    * Skip gpg signing if deploy should be skipped

--- continuous-deployment/before-deploy.sh
+++ continuous-deployment/before-deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # Decrypt signing key
-openssl aes-256-cbc -K $encrypted_a3a4db86818a_key -iv $encrypted_a3a4db86818a_iv \
-  -in continuous-deployment/payworks_sign_key.asc.enc -d | gpg --fast-import
+if [$MAVEN_DEPLOY_SKIP == "false"]; then
+    openssl aes-256-cbc -K $encrypted_a3a4db86818a_key -iv $encrypted_a3a4db86818a_iv \
+    -in continuous-deployment/payworks_sign_key.asc.enc -d | gpg --fast-import
+fi
