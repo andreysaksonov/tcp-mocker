18239396f786057b4d35445eff9cf2e238a29d51
Andrey Saksonov <andrey@saksonov.me>
2019-06-04 13:28:47 +0300

    fix travis PR building when secrets are not injected by travis (#12)
    See https://docs.travis-ci.com/user/pull-requests/#pull-requests-and-security-restrictions

--- continuous-deployment/before-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Decrypt signing key
-if [$MAVEN_DEPLOY_SKIP == "false"]; then
-    openssl aes-256-cbc -K $encrypted_a3a4db86818a_key -iv $encrypted_a3a4db86818a_iv \
-    -in continuous-deployment/payworks_sign_key.asc.enc -d | gpg --fast-import
-fi
